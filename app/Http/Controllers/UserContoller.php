17417e17b2b2073541691d33ce009fda47d01160
bhathiya fernando <26029390+bathiya-fernando@users.noreply.github.com>
2025-06-02 22:00:12 +0100

    Create authentication section and added messages

--- app/Http/Controllers/UserContoller.php
+++ app/Http/Controllers/UserContoller.php
@@ -4,17 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserContoller extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', 'max:50', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:20', 'confirmed'],
         ]);
-        User::create($incomingFields);
-        return 'Your account has been created';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'Your account has been created successfully. You can now log in.');
+    }
+
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt([
+            'username' => $incomingFields['loginusername'],
+            'password' => $incomingFields['loginpassword']
+        ])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You have successfully logged in');
+        } else {
+            return redirect('/')->with('error', 'Login failed. Please check your credentials and try again.');
+        }
+    }
+
+    public function showCorrectHomePage()
+    {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'You have successfully logged out');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserContoller;
 
-Route::get('/', function () {
-    return view('homepage');
-});
-
-
+Route::get('/',  [UserContoller::class, 'showCorrectHomePage']);
 Route::get('/about', function () {
     return view('single-post');
 });
-
-
-Route::post('/register', [UserContoller::class , 'register']);
+Route::post('/register', [UserContoller::class, 'register']);
+Route::post('/login', [UserContoller::class, 'login']);
+Route::post('/logout', [UserContoller::class, 'logout']);
